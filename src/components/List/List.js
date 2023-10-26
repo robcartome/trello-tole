65161a777a69860f2dcb17b7e2d7104d1b2b5b46
Robert Tolentino <robert@apurata.com>
2023-10-26 17:33:20 -0500

    Add update card and Move Modal card to List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,33 +1,67 @@
+import { useEffect, useState } from "react";
 import "./List.css";
-import Card from "../card/Card";
 import AddLine from 'remixicon-react/AddLineIcon';
+import Card from "../card/Card";
+import ModalCard from "../card/ModalCard";
 import { CardsSeeder } from "../../utils/seederDataBase";
 
-function List({dataList, setSelectCard, onClickSelectCard }){
-  const filterCards = CardsSeeder.filter(card => dataList.cardIds.includes(card.cardId));
-  return <li className="list">
-            <div className="list__header">
-              <h2  className="">{dataList.name}</h2>
-            </div>
-            <ul className="list__body">
-              {
-                filterCards.map((card) =>
-                {
-                  console.log('card', card)
-                  return <Card
-                    key={card.cardId}
-                    onClick={() => onClickSelectCard(card)}
-                    name={card.name}
-                  />
-                }
-                )
-              }
-            </ul>
-            <div className="list__footer">
-              <AddLine color="#AAACAF" /> Add new Card
-            </div>
+function List({dataList}){
+  const [ cards, setCards ] = useState([]);
+  const [ showModalCard, setShowModalCard ] = useState(false);
+  const [ selectCard, setSelectCard ] = useState(null);
 
-          </li>
+  useEffect(() => {
+    setCards(CardsSeeder);
+  }, [])
+
+  const onClickSelectCard = (card) => {
+    setSelectCard(card);
+    setShowModalCard(true);
+  }
+  const closeModalCard = () => {setShowModalCard(false)}
+  const updateCard = ({ name, description }) => {
+    const updateCards = cards.map((card) => {
+      if (card.cardId === selectCard.cardId) {
+        return {...card, "name" : name, "description" : description }
+      }
+      return card
+    });
+    setCards(updateCards);
+  }
+  const filterCards = cards.filter(card => dataList.cardIds.includes(card.cardId));
+
+  return (
+    <>
+      <li className="list">
+        <div className="list__header">
+          <h2 className="">{dataList.name}</h2>
+        </div>
+        <ul className="list__body">
+          {filterCards.map((card) => {
+            console.log("card", card);
+            return (
+              <Card
+                key={card.cardId}
+                onClick={() => onClickSelectCard(card)}
+                name={card.name}
+              />
+            );
+          })}
+        </ul>
+        <div className="list__footer">
+          <AddLine color="#AAACAF" /> Add new Card
+        </div>
+      </li>
+
+      {showModalCard && (
+        <ModalCard
+          onClose={closeModalCard}
+          card={selectCard}
+          updateCard={updateCard}
+        />
+      )}
+    </>
+  );
 }
 
 export default List;
--- src/components/board/ShowBoard.js
+++ src/components/board/ShowBoard.js
@@ -1,20 +1,14 @@
-import AddLine from 'remixicon-react/AddLineIcon';
-import user1 from "../../assets/images/userw.png";
-import List from '../list/List';
-import "./ShowBoard.css";
 import { useState } from "react";
-import ShowCard from "../card/ShowCard";
+import AddLine from 'remixicon-react/AddLineIcon';
 import AddForm from '../UI/forms/AddForm';
+import List from '../List/List';
+import user1 from "../../assets/images/userw.png";
 
-function ShowBoard({setGo, listData, nameBoard, addList, idBoard}){
-  const [ currentCard, setCurrentCard ] = useState(null);
+import "./ShowBoard.css";
+
+
+function ShowBoard({setGo, idBoard, list, nameBoard, addList}){
   const [ showAddFormList, setShowAddList ] = useState(false);
-  /* console.log("ss-", currentCard) */
-  const showModalCard = () => {
-    return (currentCard && (<ShowCard  setCurrentCard= {setCurrentCard}/>)
-    );
-  }
-
   return (
     <section className="lists-container">
       {showAddFormList && (
@@ -22,7 +16,9 @@ function ShowBoard({setGo, listData, nameBoard, addList, idBoard}){
       )}
       <h3 className="boards__title">{nameBoard}</h3>
       <ul className="lists-container__head">
-        <li className="user-photo "><AddLine color="#fff" /></li>
+        <li className="user-photo ">
+          <AddLine color="#fff" />
+        </li>
         <li className="user-photo user-photo--color">
           <span /><img src={user1} alt="user" />
         </li>
@@ -32,14 +28,13 @@ function ShowBoard({setGo, listData, nameBoard, addList, idBoard}){
       </ul>
 
       <ul className="lists-container__main">
-        <li className="button-add" onClick={()=>setShowAddList(true)}><AddLine color="#fff" /></li>
+        <li className="button-add" onClick={()=>setShowAddList(true)}>
+          <AddLine color="#fff" />
+        </li>
         {
-          listData.map(list => <List key={list.listId} dataList={list} setCurrentCard = {setCurrentCard}/>)
+          list.map(list => <List key={list.listId} dataList={list} />)
         }
       </ul>
-
-      { showModalCard() }
-
     </section>
   )
 }
